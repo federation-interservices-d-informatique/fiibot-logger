9bb5c5893f25041a0dd14fba11d473742bc6d046
Woomy <woomy@woomy.be>
2021-08-13 15:57:29 +0200

    Events: Add messageDelete logs

---
+++ src/events/messageDelete.ts
@@ -0,0 +1,38 @@
+import { fiiClient } from "@federation-interservices-d-informatique/fiibot-common";
+import { Message } from "discord.js";
+import { EventData } from "../typings/eventData";
+import { getLogChan } from "../utils/getLogChan.js";
+
+const data: EventData = {
+    name: "logmessagedelete",
+    type: "messageDelete",
+    callback: async (msg: Message): Promise<void> => {
+        if (msg.partial) await msg.fetch();
+        const logchan = await getLogChan(
+            msg.author.client as fiiClient,
+            msg.guild
+        );
+
+        if (!logchan) return;
+        logchan.send({
+            embeds: [
+                {
+                    description: `**Un message de ${msg.author} dans ${msg.channel} a été supprimé**`,
+                    color: "RED",
+                    footer: {
+                        icon_url: `${msg.guild.iconURL({ dynamic: true })}`,
+                        text: `Logs de ${msg.guild.name}`
+                    },
+                    timestamp: new Date(),
+                    fields: [
+                        {
+                            name: "Ancien contenu:",
+                            value: `\`\`\`${msg.content}\`\`\``
+                        }
+                    ]
+                }
+            ]
+        });
+    }
+};
+export default data;
