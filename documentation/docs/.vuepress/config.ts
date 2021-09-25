9a84f424d205b62f1e5faeafd6c2d1cc8b90c68d
Woomymy <github@woomy.ovh>
2021-09-25 14:15:33 +0200

    Docs: Setup vuepress

---
+++ documentation/docs/.vuepress/config.ts
@@ -0,0 +1,8 @@
+import { defineUserConfig } from "vuepress";
+import type { DefaultThemeOptions } from "vuepress";
+
+export default defineUserConfig<DefaultThemeOptions>({
+    lang: "fr-FR",
+    title: "Hello vue",
+    description: "Hello"
+});
