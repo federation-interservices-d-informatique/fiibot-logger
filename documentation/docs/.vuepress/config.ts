c7e38242cefe1322e093bd830bbaeb95f1233ceb
Woomy <woomy@woomy.be>
2021-09-29 18:43:09 +0200

    Docs: Remove docs

--- documentation/docs/.vuepress/config.ts
+++
@@ -1,8 +0,0 @@
-import { defineUserConfig } from "vuepress";
-import type { DefaultThemeOptions } from "vuepress";
-
-export default defineUserConfig<DefaultThemeOptions>({
-    lang: "fr-FR",
-    title: "Hello vue",
-    description: "Hello"
-});
