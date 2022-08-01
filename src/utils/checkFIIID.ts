9deb29f2ab7061012f86bf9c0c2f8c46bbe8f4e3
Woomymy <github@woomy.ovh>
2022-08-01 15:42:47 +0200

    CI: Add docker push CI workflow
    Signed-off-by: Woomymy <github@woomy.ovh>

--- src/utils/checkFIIID.ts
+++ src/utils/checkFIIID.ts
@@ -3,6 +3,4 @@ import { ID_REGEX } from "../Constants.js";
 /**
  * Check if an FII ID is present in the message
  */
-export const checkFIIID = (content: string): boolean => {
-    return ID_REGEX.test(content);
-};
+export const checkFIIID = (content: string): boolean => ID_REGEX.test(content);
