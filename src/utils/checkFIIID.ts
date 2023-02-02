2e771d34f785d0eee52f78a503a3e041f481b1cd
Woomy <woomy@woomy.be>
2023-02-02 21:59:11 +0100

    util/checkFIIID: Use common FII_ID constant
    Signed-off-by: Woomymy <woomy@woomy.be>

--- src/utils/checkFIIID.ts
+++ src/utils/checkFIIID.ts
@@ -1,6 +1,7 @@
-import { ID_REGEX } from "../Constants.js";
+import { FII_ID_REGEX } from "@federation-interservices-d-informatique/fiibot-common";
 
 /**
  * Check if an FII ID is present in the message
  */
-export const checkFIIID = (content: string): boolean => ID_REGEX.test(content);
+export const checkFIIID = (content: string): boolean =>
+    FII_ID_REGEX.test(content);
